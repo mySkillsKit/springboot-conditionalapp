93f16804c99c369281c7f665c1bb98a012406781
Anatolii Vasilev <avas2012@gmail.com>
2022-07-21 23:38:30 +0400

    added @Value

--- src/main/java/ru/netology/springbootconditionalapp/controller/HelloController.java
+++ src/main/java/ru/netology/springbootconditionalapp/controller/HelloController.java
@@ -1,18 +1,14 @@
 package ru.netology.springbootconditionalapp.controller;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConfigurationProperties("hello")
 public class HelloController {
 
+    @Value("${hello.from:undefined}")
     private String from;
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
 
     @GetMapping("/")
     private String hello() {
